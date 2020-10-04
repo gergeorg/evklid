695e3eecb8ae4166af83dfe80297f7dae91a78a1
gergeorg <62167850+gergeorg@users.noreply.github.com>
2020-10-04 13:09:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
-window.addEventListener('DOMContentLoaded', function() {
-
-})
+const menuOpen = document.querySelector('.menu__btn');
+const nav = document.querySelector('.navigation');
+const closeMenu = document.querySelector('.menu__btn--close ');
+const navLink = document.querySelectorAll('.nav-link');
+const btns = document.querySelectorAll('.questions-btn');
+const questionsSubtitle = document.querySelectorAll('.questions-subtitle');
 
 //menu
 
-document.querySelector('.menu__btn').addEventListener('click', function() {
-  document.querySelector('.nav').classList.toggle('menu-active')
+menuOpen.addEventListener('click', function(){
+  nav.classList.add('menu-active')
 })
 
+closeMenu.addEventListener('click', function(){
+  nav.classList.remove('menu-active')
+})
+
+navLink.forEach(function(el){
+  el.addEventListener('click', function(){
+    nav.classList.remove('menu-active')
+  })
+})
+
+//scroll menu
+
+$(document).ready(function() {
+  $('nav a').on('click', function(event){
+    event.preventDefault();
+
+    const href = $(this).attr('href');
+    const offset = $(href).offset().top;
+
+    $('body,html').animate({
+      scrollTop: offset,
+    }, 700);
+  });
+});
 
 
 //slider
@@ -38,8 +65,19 @@ $( "#accordion" ).accordion({
   active: 5
 });
 
-document.querySelectorAll('.questions-subtitle').forEach(function(el) {
+// change + / -
+
+questionsSubtitle.forEach(function(el) {
+  const btn = el.querySelector('.questions-btn')
   el.addEventListener('click', function(e) {
-    e.target.querySelector('.questions-btn').classList.toggle('accordion-btn__active')
+    btns.forEach(item => {
+      item.classList.remove('accordion-btn__active')
+    })
+
+    if (el.classList.contains('ui-accordion-header-active')) {
+      btn.classList.add('accordion-btn__active')
+      } else {
+      btn.classList.remove('accordion-btn__active')
+      }
   })
 })
